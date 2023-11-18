dd2e84565a885cec81444e37ec711f6db51b6e80
mir21bek <mirbek.2121@gmail.com>
2023-11-18 17:53:44 +0600

    get schema

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from rest_framework.schemas import openapi, get_schema_view
+
+from config import settings
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="This is API for future NeoCaffee CRM systems",
+      default_version='v1',
+      description="Please enjoy using this API end keep calm )) with love your teammates Mirbek and Asel",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api-users/', include('users.urls')),
+    path('api-menu/', include('menu.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
